534ec602113150a185cd8e87cf5c37966509bac6
djarae <djaraestivill@gmail.com>
2021-05-16 23:25:16 -0400

    4: Chronometer functionality in progress

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
 // React
-import React, { useState, useEffect } from "react";
-import Routes from "./Routes";
+import React, { useState} from "react";
+// import Routes from "./Routes";
 // Styles
 import "./App.css";
 
-
-
-//Functions of the program
-
-async function chronometerStart(){
-  const result = await chronometerAsync();
-  console.log(result);
-}
-
-function chronometerAsync(){
-for (let i=0;i<10;i++){
-    console.log("Hello"); console.log(i);
-    setTimeout(() => {  console.log("World!"); }, 2000);  
-}
-return 0
-}
-
+let globalEndCronometer = false;
+let i = 0;
 
 function App() {
-    const [cronometer, setCronometrer] = useState(0);
+    const [cronometer, setCronometer] = useState(0);
 
+    async function chronometerStart(){
+      globalEndCronometer = false;
+      while ((globalEndCronometer===false)){
+        // console.log("value of endCronometer is ");console.log(globalEndCronometer);
+        setCronometer(i + 1);
+        // console.log("cronometer value " + cronometer)
+        i = i + 1;
+        await delay(1);
+      }
+      return 0 ;
+    }
 
-  function probarApi(){
-    console.log("Estamos en react probaremos la api")
-    fetch('http://localhost:3000/notes')
-    .then(response => response.json())
-    .then(data => this.setState({ totalReactPackages: data.total }));
-  }
+    function chronometerPause(){
+      globalEndCronometer = true;
+    }
+
+    function chronometerEnd(){
+      // console.log("entro al end cronometer ");
+      globalEndCronometer = true;
+      // console.log(globalEndCronometer);
+      i = 0;
+      setCronometer(0);
+    }
+    
+    function delay(n){
+      return new Promise(function(resolve){
+          setTimeout(resolve,n*1000);
+      });
+    }
+  // function probarApi(){
+  //   console.log("Estamos en react probaremos la api")
+  //   fetch('http://localhost:3000/notes')
+  //   .then(response => response.json())
+  //   .then(data => this.setState({ totalReactPackages: data.total }));
+  // }
  
   return (
-
-      <div onClick={probarApi}>
+      <div>
            {/* <Routes /> */}
            <h1> APP DE CRONOMETRO</h1>
-
-           <button  onClick={chronometerStart()}>Chronometer start</button>
+           <h1>{cronometer}</h1>
+           <button  onClick={() => chronometerStart()}>START</button>
+           <button  onClick={() => chronometerPause()}>PAUSE</button>
+           <button  onClick={() => chronometerEnd()}>END</button>
            {/* <button onClick={probarApi()}>PROBAR API</button> */}
       </div>
   );
