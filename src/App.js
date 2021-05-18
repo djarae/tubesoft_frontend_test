df9e7b7facc24cb7aeadcdb8d3fdbaac8bfa2726
djarae <djaraestivill@gmail.com>
2021-05-18 18:52:52 -0400

    11: Chronometer functionality advancing proceding to format the text to send to DB

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import { makeStyles } from '@material-ui/core/styles';
 // Styles
 import "./App.css";
 
-let globalEndCronometer = false;
+let endCronometer = false;
 let i = 0;
+let j= 0;
+let k = 0;
 
 function App() {
 //Styles
@@ -37,32 +39,53 @@ function App() {
 
   
 //Hooks
-    const [cronometer, setCronometer] = useState(0);
+    const [cronometerHr, setCronometerHr] = useState(0);
+    const [cronometerMin, setCronometerMin] = useState(0);
+    const [cronometerSec, setCronometerSec] = useState(0);
+    const [auxHr,setAuxHr] = useState("0");
+    const [auxMin,setAuxMin] = useState("0");
+    const [auxSec,setAuxSec] = useState("0");
+    
+  
     const classes        = useStyles();
 
 //JS
     async function chronometerStart(){
-      globalEndCronometer = false;
-      while ((globalEndCronometer===false)){
-        // console.log("value of endCronometer is ");console.log(globalEndCronometer);
-        setCronometer(i + 1);
+      const sesenta = 3;
+      const cero = 0;
+      endCronometer = false;
+      while ((endCronometer===false)){
+        // console.log("value of endCronometer is ");console.log(endCronometer);
+        if (i!=sesenta){setCronometerSec(i + 1)}
         // console.log("cronometer value " + cronometer)
         i = i + 1;
+        if (i>9){setAuxSec("")}
+        if (j>9){setAuxMin("")}
+        if (k>9){setAuxHr("")}
+        console.log("resultado divi");console.log(i%3)
+        if (i%sesenta==0){setCronometerMin(j+1); j=j+1}
+        if ( (j%sesenta==0) & (j>0)){  setCronometerHr(k+1); k=k+1}
+        if (i==sesenta){ setCronometerSec(cero);i=0  }
+        if (j==sesenta){ setCronometerMin(cero);j=0 }
         await delay(1);
       }
       return 0 ;
     }
 
     function chronometerPause(){
-      globalEndCronometer = true;
+      endCronometer = true;
     }
 
     function chronometerEnd(){
       // console.log("entro al end cronometer ");
-      globalEndCronometer = true;
-      // console.log(globalEndCronometer);
+      endCronometer = true;
+      // console.log(endCronometer);
       i = 0;
-      setCronometer(0);
+      j=0;
+      k=0;
+      setCronometerSec(0);
+      setCronometerMin(0);
+      setCronometerHr(0);
     }
     
     function delay(n){
@@ -70,22 +93,32 @@ function App() {
           setTimeout(resolve,n*1000);
       });
     }
-  // function probarApi(){
-  //   console.log("Estamos en react probaremos la api")
-  //   fetch('http://localhost:3000/notes')
-  //   .then(response => response.json())
-  //   .then(data => this.setState({ totalReactPackages: data.total }));
-  // }
+
+    function probarApi(){
+      console.log("Estamos en react probaremos la api dx")
+
+      const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify({ id: 9,duration: "16" })
+      };
+
+      console.log("requestopt valen")
+      console.log(requestOptions);
+
+      fetch('http://localhost:8070/chronometers',requestOptions)
+      .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
+    }
  
   return (
       <Grid container spacing={250} className={classes.grid}>
         <Container className={classes.container}>
             <h1> APP DE CRONOMETRO</h1>
-            <h1>{cronometer}</h1>
+            <h1>{auxHr}{cronometerHr}:{auxMin}{cronometerMin}:{auxSec}{cronometerSec}</h1>
                 <Button  className={classes.button} onClick={() => chronometerStart()}>START</Button>
                 <Button  className={classes.button} onClick={() => chronometerPause()}>PAUSE</Button>
                 <Button  className={classes.button} onClick={() => chronometerEnd()}>END</Button>
-            {/* <button onClick={probarApi()}>PROBAR API</button> */}
+            <button onClick={() => probarApi()}>PROBAR API</button>
         </Container>
       </Grid>
   );
